bb76f432cd64e543a2d7c626879006e35b45043a
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-06 08:54:55 +0200

    Initial Commit

---
+++ FakeStoreApiMVVMAproach/FakeStoreApiMVVMAproachApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApiMVVMAproachApp.swift
+//  FakeStoreApiMVVMAproach
+//
+//  Created by Buhle Radzilani on 2024/06/06.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApiMVVMAproachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FakeStoreApiMVVMAproach/Services/Webservice.swift
@@ -0,0 +1,10 @@
+//
+//  Webservice.swift
+//  FakeStoreApiMVVMAproach
+//
+//  Created by Buhle Radzilani on 2024/06/06.
+//
+
+import Foundation
+
+//This web service wil be responsible for getting the data
